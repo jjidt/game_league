8075119d2d84669bfda8e750e7c792ebd89c041b
JJ Idt <johnidt@gmail.com>
2014-08-15 16:00:11 -0700

    validate presence of score on player creation

--- lib/player.rb
+++ lib/player.rb
@@ -1,3 +1,4 @@
 class Player < ActiveRecord::Base
   validates :name, :presence => true
+  validates :score, :presence => true
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe Player do
-  it {should validate_presence_of :name}
+  it {should validate_presence_of :name}  
+  it {should validate_presence_of :score}
 
 end
