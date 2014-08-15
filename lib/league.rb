8ca4e564eeb1ef3a86edd5436d39ceed6896a012
JJ Idt <johnidt@gmail.com>
2014-08-15 16:36:35 -0700

    validate presence of league name

---
+++ lib/league.rb
@@ -0,0 +1,4 @@
+class League < ActiveRecord::Base
+  validates :name, :presence => true
+
+end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
 describe Player do
-  it {should validate_presence_of :name}
-  it {should validate_presence_of :score}
-  it {should allow_value(21).for(:score)}
-  it {should allow_value(0).for(:score)}
-  it {should_not allow_value(22).for(:score)}
-  it {should have_and_belong_to_many :games}
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :score }
+  it { should allow_value(21).for(:score) }
+  it { should allow_value(0).for(:score) }
+  it { should_not allow_value(22).for(:score) }
+  it { should have_and_belong_to_many :games }
 end
