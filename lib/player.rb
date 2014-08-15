4239fa773aac528be5b73852e59863af1d611796
JJ Idt <johnidt@gmail.com>
2014-08-15 16:18:06 -0700

    add validation test for player name and score range

--- lib/player.rb
+++ lib/player.rb
@@ -1,4 +1,5 @@
 class Player < ActiveRecord::Base
   validates :name, :presence => true
   validates :score, :presence => true
+  validates_inclusion_of :score, in: 0..21
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 
 describe Player do
-  it {should validate_presence_of :name}  
+  it {should validate_presence_of :name}
   it {should validate_presence_of :score}
-
+  it {should allow_value(21).for(:score)}
+  it {should allow_value(0).for(:score)}
+  it {should_not allow_value(22).for(:score)}
 end
