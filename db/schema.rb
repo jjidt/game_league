de3e1c24ad69c75f4a87a98a9ecfc9f522907e75
JJ Idt <johnidt@gmail.com>
2014-08-16 13:16:24 -0700

    add timestamp to games table

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140815224756) do
+ActiveRecord::Schema.define(version: 20140816182856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "games", force: true do |t|
-    t.integer "league_id"
+    t.integer  "league_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "games_players", id: false, force: true do |t|
