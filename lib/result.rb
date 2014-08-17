95156c28fe200c292410333d6bdb2cf75c498227
JJ Idt <johnidt@gmail.com>
2014-08-16 22:37:42 -0700

    assign win status to result before save

--- lib/result.rb
+++ lib/result.rb
@@ -4,4 +4,17 @@ class Result < ActiveRecord::Base
 
   validates :score, :presence => true
   validates_inclusion_of :score, :in => 0..21
+
+  before_save :assign_win
+
+private
+
+  def assign_win
+    if self.score == 21
+      self.winner = true
+    else
+      self.winner = false
+    end
+  end
+
 end
--- spec/result_spec.rb
+++ spec/result_spec.rb
@@ -5,4 +5,13 @@ describe Result do
   it { should allow_value(21).for(:score) }
   it { should allow_value(0).for(:score) }
   it { should_not allow_value(22).for(:score) }
+  it 'should update winner status to true if score is equal to 21' do
+    player1_id = Player.create(:name => 'bill').id
+    player2_id = Player.create(:name => 'ted').id
+    test_game_id = Game.create.id
+    test_result = Result.create(:players_id => player1_id, :games_id => test_game_id, :score => 21)
+    test_result2 = Result.create(:players_id => player2_id, :games_id => test_game_id, :score => 7)
+    expect(test_result.winner).to eq true
+    expect(test_result2.winner).to eq false
+  end
 end
